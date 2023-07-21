1194cf4148c35df837f6aee9a8cb3d22f980b3ae
ayakuma309 <ayako.30901@gmail.com>
2023-07-21 21:32:23 +0900

    feat: 動画再生されるか確認できた

--- src/pages/posts/[id].tsx
+++ src/pages/posts/[id].tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import apiClient from '@/lib/apiClient';
 import { PostType } from '@/types/types';
 import { GetServerSideProps } from 'next';
+import Youtube from 'react-youtube'
 
 type Props = {
   post: PostType
@@ -30,11 +31,7 @@ const PostDetail = ({post}: Props ) => {
     <div className="container mx-auto px-4 py-8">
       <div className="bg-white shadow-md rounded p-4 mb-4">
         <div className="mb-4">
-          <img
-            src={post.url}
-            className="w-100 h-100 rounded-md  mx-auto"
-            alt="youtube image"
-          />
+          <Youtube videoId={post.videoId} className="w-100 h-100 rounded-md  mx-auto"/>
           <p>{post.title}</p>
           <p className="text-gray-700">{post.description}</p>
         </div>
