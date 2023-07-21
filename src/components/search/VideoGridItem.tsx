0fa17b8e0647bde95402a5af9d53314a8aa3d42d
ayakuma309 <ayako.30901@gmail.com>
2023-07-21 23:43:08 +0900

    feat: tagも追加できるようにした

--- src/components/search/VideoGridItem.tsx
+++ src/components/search/VideoGridItem.tsx
@@ -1,31 +1,47 @@
 import React, { useState } from 'react'
 import apiClient from '@/lib/apiClient';
 import { useRouter } from 'next/router';
+import Select from 'react-select';
+import tagOptions from '@/json/tag.json'
 interface VideoGridItemProps {
   id: string;
   src: string;
   title: string;
 }
+
+interface OptionType {
+  value: string;
+  label: string;
+}
+
 const VideoGridItem:React.FC<VideoGridItemProps> = ({id,src,title}) => {
   const [postText, setPostText] = useState('');
+  const [selectedTags, setSelectedTags] = useState<OptionType[]>([]);
+
   const router = useRouter();
+
   const handleSubmitPost = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
+      const selectedTagNames = selectedTags.map((tag) => tag.label);
+
       await apiClient.post("/posts/post", {
         videoId : id,
         url: src,
         title: title,
         description: postText,
+        tags: selectedTagNames,
       });
 
       // 投稿後、投稿フォームのテキストエリアをクリア
       setPostText("");
+      setSelectedTags([]);
       router.push("/");
     } catch (err) {
       alert("入力内容が正しくありません。");
     }
   };
+
 
   return (
     <div className='flex flex-col' key={id}>
@@ -38,6 +54,13 @@ const VideoGridItem:React.FC<VideoGridItemProps> = ({id,src,title}) => {
           value={postText}
           onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setPostText(e.target.value)}
         ></textarea>
+        <Select
+          isMulti
+          options={tagOptions}
+          value={selectedTags}
+          onChange={(selectedTags) => setSelectedTags(selectedTags as OptionType[])}
+          placeholder="タグを選択してください"
+        />
         <button
           type="submit"
           className="mt-2 bg-gray-700 hover:bg-green-700 duration-200 text-white font-semibold py-2 px-4 rounded"
