981601a5db7b02be4b833539016ef80202b2df6f
ayakuma309 <ayako.30901@gmail.com>
2023-07-24 18:55:21 +0900

    feat: 共有機能をランダムページにも追加

--- src/components/random/RandomPost.tsx
+++ src/components/random/RandomPost.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import { PostType } from '@/types/types';
+import { TwitterIcon, TwitterShareButton } from 'react-share';
 
 type Props = {
   post: PostType
@@ -9,6 +10,16 @@ type Props = {
 const RandomPost:React.FC<Props> = ({post}) => {
   return (
     <div style={{width: "300px"}}>
+      <TwitterShareButton
+        title={`${post.title}`}
+        hashtags={["校長への感謝"]}
+        url={`https://www.youtube.com/watch?v=${post.videoId}`}
+      >
+        <TwitterIcon
+          className="text-white font-bold rounded-full mr-5"
+          size={"32px"}
+        />
+      </TwitterShareButton>
       <div className="bg-white shadow-md rounded p-4 m-1 ">
         <div>
           <Link href={`/posts/${post.id}`}>
