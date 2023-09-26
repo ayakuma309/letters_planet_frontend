d96c6d879a6e33dfd3d0df38e64f5b0d2037b653
ayakuma309 <ayako.30901@gmail.com>
2023-09-26 17:51:41 +0900

    fix: コメント修正

--- src/components/random/RandomPost.tsx
+++ src/components/random/RandomPost.tsx
@@ -10,17 +10,6 @@ type Props = {
 const RandomPost: React.FC<Props> = ({ post }) => {
   return (
     <div style={{ width: "300px" }}>
-      <TwitterShareButton
-        title={`${post.title}`}
-        hashtags={["校長への感謝"]}
-        url={`https://www.youtube.com/watch?v=${post.videoId}`}
-      >
-        <div
-          className="text-white font-bold rounded-full mr-5 bg-black  py-1 px-2"
-        >
-          \uD835\uDD4F
-        </div>
-      </TwitterShareButton>
       <div className="bg-white shadow-md rounded p-4 m-1 ">
         <div>
           <Link href={`/posts/${post.id}`}>
