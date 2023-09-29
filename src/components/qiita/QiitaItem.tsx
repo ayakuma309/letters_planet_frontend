f2232a81121714ed6d0e078a0a5d7de8df42b754
ayakuma309 <ayako.30901@gmail.com>
2023-09-29 12:07:05 +0900

    add: Qiita記事のタグ取得 #34

--- src/components/qiita/QiitaItem.tsx
+++ src/components/qiita/QiitaItem.tsx
@@ -13,7 +13,16 @@ const QiitaItem: React.FC<Props> = ({item}) => {
           {item.title}
         </a>
       </p>
-      <div className='flex items-center'>
+      <div className="flex flex-wrap items-center justify-center">
+        {item.tags.length > 0 && item.tags.map((tag, index) => (
+          <span
+            key={index}
+            className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            {tag.name}
+          </span>
+        ))}
+      </div>
+      <div className='flex items-center mt-3'>
         <img
           className="w-10 h-10 rounded-full mr-4"
           alt="User Avatar"
