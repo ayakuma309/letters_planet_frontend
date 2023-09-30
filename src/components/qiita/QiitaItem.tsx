67d46aa498ed11826ccbfbb355643403337905e8
ayakuma309 <ayako.30901@gmail.com>
2023-09-30 14:48:13 +0900

    feat: Qiita記事保存 #34

--- src/components/qiita/QiitaItem.tsx
+++ src/components/qiita/QiitaItem.tsx
@@ -1,11 +1,39 @@
+import apiClient from '@/lib/apiClient'
 import { QiitaItemsProps } from '@/types/qiitaTypes'
+import { useRouter } from 'next/router'
 import React from 'react'
+import { toast } from 'react-toastify'
 
 type Props = {
   item: QiitaItemsProps
 }
 
 const QiitaItem: React.FC<Props> = ({item}) => {
+  const router = useRouter();
+
+  // Qiita記事をDBに保存する
+  const handleSubmitQiita = async () => {
+    try {
+      // Qiita記事のタグ名を取得
+      const qiitaTagNames = item.tags.map((tag) => tag.name);
+
+      // 投稿
+      await apiClient.post("/qiitas/qiita",{
+        qiitaId: item.id,
+        title: item.title,
+        url: item.url,
+        userName: item.user.name || "none",
+        profileImageUrl: item.user.profile_image_url,
+        tags: qiitaTagNames,
+      });
+      router.push("/qiita");
+      toast.success("投稿が完了しました");
+    } catch (err) {
+      toast.error("投稿に失敗しました");
+    }
+  }
+
+
   return (
     <div className="mb-3 py-3 px-8  rounded-lg shadow-lg w-10/12">
       <p className="text-center font-bold  text-blue-800 mb-4">
@@ -22,13 +50,22 @@ const QiitaItem: React.FC<Props> = ({item}) => {
           </span>
         ))}
       </div>
-      <div className='flex items-center mt-3'>
-        <img
-          className="w-10 h-10 rounded-full mr-4"
-          alt="User Avatar"
-          src={item.user.profile_image_url}
-        />
-        <p className="text-gray-700">{item.user.name}</p>
+      <div className='flex items-center mt-3 justify-between'>
+        <div>
+          <img
+            className="w-10 h-10 rounded-full mr-4"
+            alt="User Avatar"
+            src={item.user.profile_image_url}
+          />
+          <p className="text-gray-700">{item.user.name}</p>
+        </div>
+        <button
+          type="submit"
+          className="mt-2 bg-gray-700 hover:bg-green-800 duration-200 text-white font-semibold py-2 px-3 rounded-lg hover:shadow-lg block"
+          onClick={handleSubmitQiita}
+        >
+          保存
+        </button>
       </div>
     </div>
   )
