523436833416a6351810dcf334dc77c96647741f
ayakuma309 <ayako.30901@gmail.com>
2023-10-13 13:16:05 +0900

    fix: topページの修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,32 +1,38 @@
-import Link from 'next/link';
 import React from 'react';
 
 export default function Home() {
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
+      className={`flex  flex-col min-h-screen items-center justify-center p-24`}
     >
-      <div className='hero max-h-full'>
-        <div className='hero-overlay bg-opacity-10 sm:bg-opacity-30'>
-          <div className='hero-content text-center text-neutral-content'>
-            <div className='max-w-lg lg:max-w-xl'>
-              <h1 className='md:mb-5 text-xl md:text-2xl lg:text-3xl font-bold'>
-                何をすべきか、わからない
-                <br />
-                何したらいいのか
-              </h1>
-              <p className='mt-3 mb-5 md:text-lg sm:flex'>
-                そんなあなたを助けてくれます。
-                <br />
-                そしてメッセージを送って
-                <br />
-                あなたの気持ちを伝えることができます。
-              </p>
-              <Link href='/random_page'>
-                <button className='p-4 rounded-md btn border-none sm:inline-block bg-gradient-to-r from-orange-400 via-yellow-300 to-white hover:bg-gradient-to-r hover:from-white hover:via-yellow-300 hover:to-orange-400'>
-                  覗いてみる
-                </button>
-              </Link>
+      <div className='text-gray-600'>
+        <div className='flex flex-col  items-center text-center justify-center'>
+          <h1 className='sm:text-7xl text-5xl mb-4 font-medium text-red-500'>
+            Letters Tube
+          </h1>
+          <p className='leading-relaxed'>
+            未経験の方がどのように勉強すればいいのかを解決する
+            <br />
+            情報収集サービスです
+          </p>
+        </div>
+      </div>
+      <div className='hero-overlay bg-opacity-10 sm:bg-opacity-30'>
+        <div className='hero-content text-center text-neutral-content'>
+          <div className='max-w-lg lg:max-w-xl'>
+            <div>
+              <div className='container mx-auto'>
+                <div className='flex md:items-center md:flex-row flex-col mt-12 md:mb-64 mb-32'>
+                  <div className='md:flex-1'>
+                    <h1 className='text-3xl my-2'>簡単に情報収集できる</h1>
+                    <p className='text-gray-500 md:mb-0 '>
+                      何からはじめたらいいか悩んだことはありませんか？
+                      <br />
+                      安心してください。情報はここにあります。
+                    </p>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
