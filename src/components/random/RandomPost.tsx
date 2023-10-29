1079804533e1f8a3ceb50a95b513fc3ac33b199e
ayakuma309 <ayako.30901@gmail.com>
2023-10-30 08:53:02 +0900

    fix: 使わないコードの削除

--- src/components/random/RandomPost.tsx
+++ src/components/random/RandomPost.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
 import { PostType } from '@/types/types';
-import { TwitterShareButton } from 'react-share';
 
 type Props = {
   post: PostType;
